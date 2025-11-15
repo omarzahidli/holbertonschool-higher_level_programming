1a0be4982722f55c2019019e1cc92a1a0c9103f3
omarzahidli <zahidliomer@gmail.com>
2025-11-15 15:59:00 +0000

    Add 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-numbers = []
 for i in range(0, 100):
     s = "{:02}".format(i)
-    if  s[0] < s[1] :
-        numbers.append(s)
-print(", ".join(numbers))
+    if s[0] < s[1]:
+        if i == 89:  # last number that satisfies s[0] < s[1]
+            print(s)
+        else:
+            print(s, end=", ")
