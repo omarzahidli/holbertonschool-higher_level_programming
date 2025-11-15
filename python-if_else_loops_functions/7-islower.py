488570a7f1ccc5b6e4ba5654ef5ce98b870733a5
omarzahidli <zahidliomer@gmail.com>
2025-11-15 16:07:48 +0000

    Add 7-islower.py

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def check_case(c):
-    if 'a' <= c <= 'z':
-        return f"{c} is lower"
-    elif 'A' <= c <= 'Z':
-        return f"{c} is upper"
+def islower(c):
+    return ord('a') <= ord(c) <= ord('z')
