7935fa7af9162732be1805b564b2d70672c48cce
omarzahidli <zahidliomer@gmail.com>
2025-11-15 13:58:30 +0000

    Add 8-concat_edges.py

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+word = str.split()
+str = f"{word[5]} {word[6]} {word[12]} {word[0]}"
+print(str)
