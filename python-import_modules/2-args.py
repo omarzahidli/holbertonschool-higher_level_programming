81ae541cc5b9ab5a4d40af8493fee023f69261f0
root <root@ZH.localdomain>
2025-11-19 03:54:22 -0800

    Add 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 import sys
 def main():
+
+
     arg_list = sys.argv[1:]
     if arg_list:
         if len(arg_list) == 1:
             print(f"{len(arg_list)} argument:")
         else:
             print(f"{len(arg_list)} arguments:")
-        for index,l in enumerate(arg_list):
+        for index, l in enumerate(arg_list):
             print(f"{index+1}: {l}")
     else:
         print("0 arguments.")
