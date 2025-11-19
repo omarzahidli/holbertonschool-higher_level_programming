d62c3dd4b64652d1bd3f55779362bac4626eb6bb
omarzahidli <zahidliomer@gmail.com>
2025-11-19 14:25:38 +0000

    Add 5-main.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-        new_string = my_string.replace("c", "")
-        new_string = my_string.replace("C", "")
-        return new_string
+    result = ""
+    for char in my_string:
+        if char != "c" and char != "C":
+            result += char
+    return result
