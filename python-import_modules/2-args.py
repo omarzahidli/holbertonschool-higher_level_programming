f935c48a26bd5640819e96d10c5daab83be0b6b2
omarzahidli <zahidliomer@gmail.com>
2025-11-19 03:28:04 -0800

    Add 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
+import sys
+def main():
     arg_list = sys.argv[1:]
     if arg_list:
         if len(arg_list[1:]) > 1:
             print(f"{len(arg_list)} arguments:")
         else:
             print(f"{len(arg_list)} argument:")
-        # for i in range(len(arg_list)):
-        #     print(f"{i}: {arg_list[i]}")
         for index,l in enumerate(arg_list):
             print(f"{index+1}: {l}")
     else:
         print("0 arguments.")
+if __name__ == "__main__":
+    main()
