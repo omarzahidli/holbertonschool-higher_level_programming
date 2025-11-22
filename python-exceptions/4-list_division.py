e1203be985f434e2ce98acb8148fed71bde8576b
omarzahidli <zahidliomer@gmail.com>
2025-11-22 17:24:56 +0000

    AUTO COMMIT

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-
+    """Divides two lists element by element.
+    Args:
+        my_list_1 (list): first list.
+        my_list_2 (list): second list.
+        list_length (int): The number of elements to divide.
+    Returns:
+        A new list of length list_length containing all the divisions.
+    """
     new_list = []
-    for i in range(list_length):
+    for i in range(0, list_length):
         try:
-            a = my_list_1[i]
-            b = my_list_2[i]
-
-            if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-                print("wrong type")
-                new_list.append(0)
-            else:
-                new_list.append(a / b)
+            div = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            div = 0
         except ZeroDivisionError:
             print("division by 0")
-            new_list.append(0)
+            div = 0
         except IndexError:
             print("out of range")
-            new_list.append(0)
-    return new_list
+            div = 0
+        finally:
+            new_list.append(div)
+    return (new_list)
