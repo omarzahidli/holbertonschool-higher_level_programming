8816fe5ad095335eed8efbf2e8a11a4aabc838c3
omarzahidli <zahidliomer@gmail.com>
2025-11-26 10:39:01 +0000

    AUTO COMMIT

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-
 """ Defines Rectangle """
+
+
 class Rectangle():
 
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-    
+
     def width(self):
         """Retrieve the width."""
         self.__width
@@ -15,23 +16,23 @@ class Rectangle():
         """Set the width with validation."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        
+
         if value < 0: 
             raise ValueError("width must be >= 0")
 
         self.__width = value
-    
+
     def height(self):
         """Retrieve the height."""
         self.__height
-    
+
     def height(self, value):
         """Set the height with validation."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        
+
         if value < 0;
             raise ValueError("height must be >= 0")
-        
+
         self.__height = value
 
