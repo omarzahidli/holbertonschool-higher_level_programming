23dc6a6e106665378c2e7381385d010c36ce216b
omarzahidli <zahidliomer@gmail.com>
2025-11-26 11:04:37 +0000

    AUTO COMMIT

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+
+class Rectangle():
+    """ Defines rectangle """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("value must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("value must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    @property
+    def area(self):
+        return self.__width * self.__height
+
+    @property
+    def perimeter(self):
+        return (self.__width * self.__height)+2
