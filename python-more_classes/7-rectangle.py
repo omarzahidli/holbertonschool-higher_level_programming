a1abc7cdb66928e9317d9c19e14b39c1cf45571b
omarzahidli <zahidliomer@gmail.com>
2025-11-26 11:45:19 +0000

    AUTO COMMIT

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -57,7 +57,8 @@ class Rectangle():
         """ Prints Rectangle with #s' """
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join([str(self.print_symbol) * self.__width for _ in range(self.__height)])
+        return "\n".join(
+                [str(self.print_symbol) * self.__width for _ in range(self.__height)]
 
     def __repr__(self):
         """Return a string representation to recreate the object."""
