44a6d104e648f56f91ff82ddc6f6415341605418
omarzahidli <zahidliomer@gmail.com>
2025-11-26 10:53:40 +0000

    AUTO COMMIT

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle():
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
 
-        if value < 0: 
+        if value < 0:
             raise ValueError("width must be >= 0")
 
         self.__width = value
@@ -42,4 +42,3 @@ class Rectangle():
             raise ValueError("height must be >= 0")
 
         self.__height = value
-
