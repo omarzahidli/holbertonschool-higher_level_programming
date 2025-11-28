e69fa8832fd78853714e2c42343a60ed3998c901
omarzahidli <zahidliomer@gmail.com>
2025-11-28 13:22:01 +0000

    AUTO COMMIT

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -58,7 +58,9 @@ class Rectangle():
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(
-            [str(self.print_symbol) * self.__width for _ in range(self.__height)]
+            [
+                str(self.print_symbol) * self.__width for _ in range(self.__height)
+            ]
         )
 
     def __repr__(self):
