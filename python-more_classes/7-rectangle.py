9346b3dc7d295631dfc4f2280494001fbe2d3906
omarzahidli <zahidliomer@gmail.com>
2025-11-28 13:24:02 +0000

    AUTO COMMIT

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -54,14 +54,15 @@ class Rectangle():
         return (self.__width + self.__height)*2
 
     def __str__(self):
-        """ Prints Rectangle with #s' """
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        return "\n".join(
-            [
-                str(self.print_symbol) * self.__width for _ in range(self.__height)
-            ]
-        )
+    """ Prints Rectangle with #s' """
+    if self.__width == 0 or self.__height == 0:
+        return ""
+    return "\n".join(
+        [
+            str(self.print_symbol) * self.__width
+            for _ in range(self.__height)
+        ]
+    )
 
     def __repr__(self):
         """Return a string representation to recreate the object."""
