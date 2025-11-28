a821cc37adc61c1d4b6359f6a0681426b8861f32
omarzahidli <zahidliomer@gmail.com>
2025-11-28 14:00:14 +0000

    AUTO COMMIT

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,4 +7,4 @@ def write_file(filename="", text=""):
     Returns the number of characters written.
     """
     with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)  
+        return f.write(text)
