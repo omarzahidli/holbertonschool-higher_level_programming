4d586b2426be5d8349e98ee5c8981bc9a1c1ae7a
omarzahidli <zahidliomer@gmail.com>
2025-11-28 15:37:13 +0000

    AUTO COMMIT

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,2 @@
 -- creates a table called first_table in the current database
-CREATE TABLE IF NOT EXISTS first_table ({
-	id int,
-	name varchar(256)
-})
+CREATE TABLE IF NOT EXISTS first_table (id int, name varchar(256))
