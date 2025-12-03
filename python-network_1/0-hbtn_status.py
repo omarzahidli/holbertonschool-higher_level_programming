4e356c3d6d0149907b315166227ceea3a86504fd
omarzahidli <zahidliomer@gmail.com>
2025-12-03 10:25:39 +0000

    AUTO COMMIT

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import urllib.request
+url = "https://intranet.hbtn.io/status"
+headers = {'cfclearance': 'true'}
+req = urllib.request.Request(url, headers)
+urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+with urllib.request.urlopen(req) as response:
+    body = response.read()
