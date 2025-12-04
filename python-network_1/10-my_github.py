33ab46ff3fd04d184db2db2593a4069f45198122
omarzahidli <zahidliomer@gmail.com>
2025-12-04 10:22:07 +0000

    AUTO COMMIT

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Display GitHub user ID using Basic Authentication"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    token = sys.argv[2]  # Personal access token
+
+    url = "https://api.github.com/user"
+
+    try:
+        response = requests.get(url, auth=(username, token))
+        response.raise_for_status()
+        user_data = response.json()
+        print(user_data.get("id"))
+    except requests.exceptions.RequestException as e:
+        print("Error: {}".format(e))
