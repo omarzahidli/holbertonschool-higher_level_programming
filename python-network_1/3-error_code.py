a3cdfc1bb831d9762a01ffa7a0c957f3dd4281a5
omarzahidli <zahidliomer@gmail.com>
2025-12-04 09:16:03 +0000

    AUTO COMMIT

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" HTTPError """
+
+import urllib.request
+import sys
+
+if __name__ = "__main__":
+    url = sys.argv[1]
+    try:
+        response = urllib.request.urlopen(url)
+    except urllib.error.HTTPError as e:
+        print(f"Error: {e.code}")
