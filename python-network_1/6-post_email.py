8bb5307c589d2fefb6d93cd6882351ca4dbfea71
omarzahidli <zahidliomer@gmail.com>
2025-12-04 09:39:47 +0000

    AUTO COMMIT

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" POST email using requests """
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    response = requests.post(url, data={'email': email})
+
+    print(response.text)
