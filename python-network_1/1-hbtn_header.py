3b61c199d4e4b63d8211de425da07a77d9fe5563
omarzahidli <zahidliomer@gmail.com>
2025-12-04 08:39:30 +0000

    AUTO COMMIT

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Fetches the X-Request-Id from the response header."""
+
 import urllib.request
 import sys
-""" Import url from argv """
-url = sys.argv[1]
 
-with urllib.request.urlopen(url) as response:
-    """ Get header """
-    head = response.headers.get("X-Request-Id")
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-print(head)
+    with urllib.request.urlopen(url) as response:
+        head = response.headers.get("X-Request-Id")
+
+    print(head)
