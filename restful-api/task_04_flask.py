36dba3950ee21e0c3d9f998ed95dd4a612d6de15
omarzahidli <zahidliomer@gmail.com>
2025-12-04 11:00:56 +0000

    AUTO COMMIT

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,27 +1,33 @@
+#!/usr/bin/python3
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+# In-memory storage for users
 users = {}
 
 
 @app.route("/")
 def home():
+    """Home route"""
     return "Welcome to the Flask API!"
 
 
 @app.route("/data")
 def get_data():
+    """Return all usernames as a list"""
     return jsonify(list(users.keys()))
 
 
 @app.route("/status")
 def status():
+    """Return API status"""
     return "OK"
 
 
 @app.route("/users/<username>")
 def get_user(username):
+    """Return user data by username"""
     user = users.get(username)
     if user:
         return jsonify(user)
@@ -31,10 +37,16 @@ def get_user(username):
 
 @app.route("/add_user", methods=["POST"])
 def add_user():
+    """Add a new user"""
     new_user = request.get_json()
     username = new_user.get("username")
+
     if not username:
         return jsonify({"error": "Username is required"}), 400
+
+    if username in users:
+        return jsonify({"error": "Username already exists"}), 400
+
     users[username] = new_user
     return jsonify({"message": "User added", "user": new_user}), 201
 
