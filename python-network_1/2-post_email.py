3456e0dc40b69c9881b9893d7e37efbfd99a4bfc
omarzahidli <zahidliomer@gmail.com>
2025-12-04 08:52:08 +0000

    AUTO COMMIT

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('utf-8')
     req = urllib.request.Request(url, data=data)
-    with urllib.request.urlopen(req) as repsonse:
-        body = reasponse.read()
+    with urllib.request.urlopen(req) as response:
+        body = response.read()
     print(body.decode("utf-8"))
