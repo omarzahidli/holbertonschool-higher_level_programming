50f2f23d66446338e8d698cbb4e6ae4d9e3e2f79
omarzahidli <zahidliomer@gmail.com>
2025-12-04 09:17:10 +0000

    AUTO COMMIT

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -7,6 +7,7 @@ import sys
 if __name__ = "__main__":
     url = sys.argv[1]
     try:
-        response = urllib.request.urlopen(url)
+        with urllib.request.urlopen(url) as response:
+            body = response.read()
     except urllib.error.HTTPError as e:
         print(f"Error: {e.code}")
