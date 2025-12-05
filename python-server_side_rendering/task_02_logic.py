12bc80b8413988df10c9ec539230723c8647a947
omarzahidli <zahidliomer@gmail.com>
2025-12-05 14:21:48 +0400

    AUTO COMMIT

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template
+import json
+import os
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route('/items')
 def items():
-    return render_template('items.html')
+    json_path = os.path.join(os.path.dirname(__file__), "items.json")
+    
+    with open(json_path, "r") as file:
+        data = json.load(file)
+
+    items_list = data.get("items", [])
+
+
+    return render_template('items.html', items=items_list)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
