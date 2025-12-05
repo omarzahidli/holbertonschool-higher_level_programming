d11a284b7cd2a67d26fa307d3fab63db969963a6
omarzahidli <zahidliomer@gmail.com>
2025-12-05 14:15:57 +0400

    AUTO COMMIT

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -2,17 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/items')
+@app.route('/')
 def home():
-    return render_template('templates/items.html')
-
-# @app.route('/about')
-# def about():
-#     return render_template('about.html')
-
-# @app.route('/contact')
-# def contact():
-#     return render_template('contact.html')
+    return render_template('index.html')
+def items():
+    return render_template('items.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
